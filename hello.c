a87ed937dbf853450c8dc7680c3ac1aec2bf4037
filipcib <123461834+filipcib@users.noreply.github.com>
2023-01-28 21:21:05 +0000

    28Jan

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@
  {
     string first = get_string("What's your first name?");
     string last = get_string("What's your last name?");
-    string middle = get_string("Hey there.")
+    string random = get_string("Hey there.")
 
 
     printf("hello, %s %s", first, last);
